a6ef6f3ea597b9dc20cd9b5061f7ebc38edc82e5
Takuya Ueda <t@tenntenn.dev>
2026-01-07 20:46:34 +0900

    Background task: remoteデ

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
+      '/parser.v1.ParserService': {
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
